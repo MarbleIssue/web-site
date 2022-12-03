50cc1aafca0217e2ce8e7ae5e95aee1eb5dcf2bf
esraguuler <marble.ymgk@gmail.com>
2022-12-04 00:43:24 +0300

    web site
    web site düzenlendi

---
+++ ymgk/ymgk/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ymgk.Models;
+using System.Data.SqlClient;
+
+
+namespace ymgk.Controllers
+{
+    public class LoginController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "Data Source=LAPTOP-1KQ0BKEU\\SQLEXPRESS;Initial Catalog=ymgk;Integrated Security=True";
+        }
+
+        [HttpPost]
+
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where usarname='" + acc.Name + "' and password'" + acc.Password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+
+        }
+    }
+}
+    
